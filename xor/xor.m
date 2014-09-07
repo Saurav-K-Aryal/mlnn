6c0e96bfea973061812b832ef45679aca720ff8b
rob-nn <aguiarlima.roberto@gmail.com>
2014-09-07 14:24:09 -0300

    nothing

--- xor/xor.m
+++ xor/xor.m
@@ -1,7 +1,7 @@
 function xor()
 
-	lambda = 0;
-	iterations = 10
+	lambda = 0.01;
+	iterations = 10000;
 	%This software is based on lecture 7 through 9 from Professor Andrew Ng of Coursera Site
 	%The training set is the truth table to the xor logic port with 2 inputs
 	training_set = [
@@ -13,10 +13,10 @@ function xor()
 	m = size(training_set, 1);
 
 
-	theta1 = rand(2,3);
-	theta2 = rand(1,3);
-	delta1 = D1 = zeros(2, 3);
-	delta2 = D2 = zeros(1, 3);
+	theta1 = rand(10,3);
+	theta2 = rand(1,11);
+	delta1 = D1 = zeros(10, 3);
+	delta2 = D2 = zeros(1, 11);
 
 	for iter = 1:iterations	
 		for training_example = 1 : m
@@ -30,18 +30,18 @@ function xor()
 
 			ldelta3 = a3 - training_set(training_example, 3);	
 			ldelta2 = theta2' * ldelta3 .* a2 .* (1 - a2);
-			ldelta2 = ldelta2([2:end],:);
+			ldelta2 = ldelta2(2:end, :);
 
 			delta2 = delta2 + ldelta3 * a2';
 			delta1 = delta1 + ldelta2 * a1';
 		end	
-
-		D2 = (1/m) * delta2;
-		D1 = (1/m) * delta1;
-
+		D2(:, 2:end) = ((1/m) * delta2 + lambda * theta2)(:, 2:end);
+		D1(:, 2:end) = ((1/m) * delta1 + lambda * theta1)(:, 2:end);
 		theta2 = theta2 + D2;
 		theta1 = theta1 + D1;
 	end		
+	
+	out = [];
 	for training_example = 1 : m
 		% begin activation
 		a1 = [1 training_set(training_example, [1 2])]';
@@ -50,8 +50,10 @@ function xor()
 		z3 = theta2 * a2;
 		a3 = 1./(1+exp(-z3));
 		% end activation
-		a3	
+		out = [out; a3];
 	end	
+
+	out
 end
 	
 xor()
