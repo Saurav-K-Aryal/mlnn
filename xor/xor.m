c45d65b93ccd4fe97da01e032b17551564cb716d
Roberto Aguiar Lima <aguiarlima.roberto@gmail.com>
2014-09-08 21:05:42 -0300

    trash

--- xor/xor.m
+++ xor/xor.m
@@ -1,9 +1,8 @@
 function xor()
 
-	lambda = 0.01;
-	iterations = 10000;
-	%This software is based on lecture 7 through 9 from Professor Andrew Ng of Coursera Site
-	%The training set is the truth table to the xor logic port with 2 inputs
+	alpha = 0.01;
+	neta = 0.5;
+	epochs = 1000;
 	training_set = [
 			0 0 0
 			0 1 1
@@ -13,47 +12,56 @@ function xor()
 	m = size(training_set, 1);
 
 
-	theta1 = rand(10,3);
-	theta2 = rand(1,11);
-	delta1 = D1 = zeros(10, 3);
-	delta2 = D2 = zeros(1, 11);
+	w1 = randn(2, 3);
+	w2 = randn(1, 3);
 
-	for iter = 1:iterations	
+	for iter = 1:	epochs
 		for training_example = 1 : m
 			% begin activation
-			a1 = [1 training_set(training_example, [1 2])]';
-			z2 = theta1 * a1;
-			a2 = [1; 1./(1+exp(-z2))];
-			z3 = theta2 * a2;
-			a3 = 1./(1+exp(-z3));
+			y0 = [1 training_set(training_example, [1 2])]';
+			v1 = zeros(2, 1);
+			for j = 1: 2
+				for i = 1:3
+					v1(j) = w1(j,i)*y0(i);
+				end
+			end
+			y1 = [1; 1./(1+exp(-v1))];
+			v2 = 0;
+			for i = 1:3
+				v2 = w2(1, i) * y1(i);
+			end
+
+			y2 = 1./(1+exp(-v2));
 			% end activation
+		
+			d2 = training_set(training_example, 3);
+			e2 = d2 - y2;
 
-			ldelta3 = a3 - training_set(training_example, 3);	
-			ldelta2 = theta2' * ldelta3 .* a2 .* (1 - a2);
-			ldelta2 = ldelta2(2:end, :);
+			ldelta2 = e2 .* y2 .* (1 - y2);	
 
-			delta2 = delta2 + ldelta3 * a2';
-			delta1 = delta1 + ldelta2 * a1';
+			ldelta = zeros(3,1);
+			for j = 1:2
+				d = y1(j+1, 1) * (1 - y1(j+1));
+				ldelta1(j+1, 1) = d * ldelta2 * w2(1, j);
+			end 
+
+			for i = 1:3
+				w2(1,i) = w2(1, i) + neta * ldelta2 * y1(i);
+			end
+
+			for i = 1:3
+				for j = 1:2
+					w1(j, i) = w1(j, i) + neta * ldelta1(j+1) * y0(i);
+				end
+			end
+			if iter == epochs
+				y2>0.5
+			end
+
 		end	
-		D2(:, 2:end) = ((1/m) * delta2 + lambda * theta2)(:, 2:end);
-		D1(:, 2:end) = ((1/m) * delta1 + lambda * theta1)(:, 2:end);
-		theta2 = theta2 + D2;
-		theta1 = theta1 + D1;
 	end		
 	
-	out = [];
-	for training_example = 1 : m
-		% begin activation
-		a1 = [1 training_set(training_example, [1 2])]';
-		z2 = theta1 * a1;
-		a2 = [1; 1./(1+exp(-z2))];
-		z3 = theta2 * a2;
-		a3 = 1./(1+exp(-z3));
-		% end activation
-		out = [out; a3];
-	end	
-
-	out
+	
 end
 	
 xor()
